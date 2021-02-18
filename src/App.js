fd079ab12410ed3399c320f7141f20550be23144
andjelarajic <andjelarajic1@gmail.com>
2021-02-18 19:49:58 +0100

    search input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import { Component } from 'react';
 import './App.css';
 import List from "../src/List/List"
+import SearchInput from "./SearchInput/SearchInput"
 
 class App extends Component {
   constructor(props) {
@@ -10,25 +11,30 @@ class App extends Component {
       people: [],
     }
     this.getPeople = this.getPeople.bind(this);
+    
   }
   getPeople() {
     return axios.get("https://swapi.dev/api/people").then((response)=> {
-      console.log(response.data.results);
       this.setState( {people:response.data.results})
+      console.log("click!")
     })
   }
+
+ 
 
 
 componentDidMount(){
   this.getPeople()
 }
 
+
   render() {
     const {people} = this.state; 
   return (
     <div className="App">
-
-
+  
+  <input className="Input1" type="text" placeholder="Enter Name" onChange={(e) => this.getPeople(e.target.value)} />
+<SearchInput/>
      <List people={people}/>
     </div>
   );
